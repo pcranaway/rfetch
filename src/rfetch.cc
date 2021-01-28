a0a01ff25014c1fbc18db64cd72fc30defefb456
pcranaway <pcranaway@tuta.io>
2021-01-28 21:22:20 +0200

    colorize

--- src/rfetch.cc
+++ src/rfetch.cc
@@ -6,13 +6,13 @@ using namespace std;
 int main() {
     Distro distro = detectDistro();
 
-    cout << "distro: " << getDistroName(distro) << "\n";
-    cout << "packages: " << countPackages(distro) << "\n";
+    cout << "\e[38;5;105mdistro: \e[0m" << getDistroName(distro) << "\e[0m\n";
+    cout << "\e[38;5;105mpackages: \e[0m" << countPackages(distro) << "\e[0m\n";
    
     string uptime = exec("uptime -p");
     replace(uptime, "up ", "");
     replace(uptime, "\n", "");
-    cout << "uptime: " << uptime << "\n";
+    cout << "\e[38;5;105muptime: \e[0m" << uptime << "\e[0m\n";
 
     return 0;
 }
