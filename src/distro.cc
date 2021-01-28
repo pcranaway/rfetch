f1227e19a0dce3d46d13f287031a0f93e93fd177
pcranaway <pcranaway@tuta.io>
2021-01-28 18:53:59 +0200

    add detectDistro method for detecting linux distributions

--- src/distro.cc
+++ src/distro.cc
@@ -13,3 +13,11 @@ enum Distro {
 static bool fileExists(std::string path) {
     return access(path.c_str(), 0) == 0;
 }
+
+static Distro detectDistro() {
+    if(fileExists("/usr/bin/dpkg")) return DEBIAN;
+    if(fileExists("/usr/bin/pacman")) return ARCH;
+    if(fileExists("/usr/bin/emerge")) return GENTOO;
+    if(fileExists("/usr/bin/dnf")) return FEDORA;
+    if(fileExists("/usr/bin/xbps-install")) return VOID;
+}
