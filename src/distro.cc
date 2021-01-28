7fa504c80cc1260f16448b7d402a9c5e373c1eef
pcranaway <pcranaway@tuta.io>
2021-01-28 19:06:07 +0200

    copy-paste exec method from stackoverflow

--- src/distro.cc
+++ src/distro.cc
@@ -1,4 +1,9 @@
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
 #include <string>
+#include <array>
 #include <unistd.h>
 
 enum Distro {
@@ -13,6 +18,21 @@ enum Distro {
 static bool fileExists(std::string path) {
     return access(path.c_str(), 0) == 0;
 }
+
+/* https://stackoverflow.com/a/478960/14674978 */
+std::string exec(const char* cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
+
 
 static Distro detectDistro() {
     if(fileExists("/usr/bin/dpkg")) return DEBIAN;
